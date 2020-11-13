27df06662e7a9dba3f590ea6d01f17aef7bb4105
AricChen <aric.chen@ecs.com.tw>
2020-11-13 16:46:30 +0800

    add RandomTool
    Signed-off-by: AricChen <aric.chen@ecs.com.tw>

---
+++ RandomTool/ConfigClass.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomTool
+{
+    public class ConfigInfo
+    {
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+        public int Count { get; set; }
+        public bool Repeatable { get; set; }
+        public bool Sort { get;set;}
+    }
+
+    public class ConfigClass
+    {
+        public static ConfigInfo OpenConfig(string CONFIG_FILE)
+        {
+            ConfigInfo configInfo = new ConfigInfo();
+            string configString;
+            try
+            {
+                using (var streamReader = new StreamReader(CONFIG_FILE, Encoding.UTF8))
+                {
+                    configString = streamReader.ReadToEnd();
+                    streamReader.Close();
+                    configInfo = JsonConvert.DeserializeObject<ConfigInfo>(configString);
+                }
+            }
+            catch (Exception)
+            {
+                configInfo.MinValue = 0;
+                configInfo.MaxValue = 100;
+                configInfo.Count = 1;
+                configInfo.Repeatable = false;
+                configInfo.Sort = false;
+                configString = JsonConvert.SerializeObject(configInfo, Formatting.Indented);       //transform json format string
+                StreamWriter streamWriter = new StreamWriter(CONFIG_FILE);    //save to config file
+                streamWriter.WriteLine(configString);
+                streamWriter.Close();
+            }
+            return configInfo;
+        }
+
+        public static void SaveConfig(ConfigInfo configInfo, string CONFIG_FILE)
+        {
+            string configString = JsonConvert.SerializeObject(configInfo, Formatting.Indented);
+            StreamWriter streamWriter = new StreamWriter(CONFIG_FILE);        //save to config file
+            streamWriter.WriteLine(configString);
+            streamWriter.Close();
+        }
+    }
+}
---
+++ RandomTool/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace RandomTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBoxRepeatable = new System.Windows.Forms.CheckBox();
+            this.checkBoxSort = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonGenerate.ForeColor = System.Drawing.Color.Blue;
+            this.buttonGenerate.Location = new System.Drawing.Point(17, 12);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(354, 35);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(12, 159);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(359, 71);
+            this.textBoxResult.TabIndex = 6;
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(17, 77);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMin.TabIndex = 1;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(139, 77);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMax.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Min Value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Max Value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "~";
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(271, 77);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCount.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Quantity: ";
+            // 
+            // checkBoxRepeatable
+            // 
+            this.checkBoxRepeatable.AutoSize = true;
+            this.checkBoxRepeatable.Location = new System.Drawing.Point(17, 115);
+            this.checkBoxRepeatable.Name = "checkBoxRepeatable";
+            this.checkBoxRepeatable.Size = new System.Drawing.Size(65, 16);
+            this.checkBoxRepeatable.TabIndex = 4;
+            this.checkBoxRepeatable.Text = "可重複?";
+            this.checkBoxRepeatable.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSort
+            // 
+            this.checkBoxSort.AutoSize = true;
+            this.checkBoxSort.Location = new System.Drawing.Point(116, 115);
+            this.checkBoxSort.Name = "checkBoxSort";
+            this.checkBoxSort.Size = new System.Drawing.Size(53, 16);
+            this.checkBoxSort.TabIndex = 5;
+            this.checkBoxSort.Text = "排序?";
+            this.checkBoxSort.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 250);
+            this.Controls.Add(this.checkBoxSort);
+            this.Controls.Add(this.checkBoxRepeatable);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.buttonGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.TextBox textBoxMax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxRepeatable;
+        private System.Windows.Forms.CheckBox checkBoxSort;
+    }
+}
+
