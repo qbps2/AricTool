5bd2cfd314ddfc1b2406f0f3f7ef14090c066913
aric.chen <aricche@ecs.com.tw>
2022-06-15 17:51:20 +0800

    add AlarmClock library and demo ap
    Signed-off-by: aric.chen <aricche@ecs.com.tw>

---
+++ AlarmDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AricPackage;
+
+Console.WriteLine("Hello, World!");
+
+int doTimeFn(string name, int id)
+{
+    Console.WriteLine("{0} {1} time out", name, id);
+    return 0;
+}
+
+AlarmClock alarm1 = new AlarmClock(doTimeFn, "Alarm AAA", 1);
+AlarmClock alarm2 = new AlarmClock(doTimeFn, "Alarm BBB", 2);
+alarm1.startAlarmClock(6);
+alarm2.startAlarmClock(5);
+
+//Thread.Sleep(30000);
+
+Console.ReadLine();
---
+++ AricAlarm/AlarmClock.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace AricPackage
+{
+    public class AlarmClock
+    {
+        private DateTime startTime;
+        private int alarmID = 0;
+        private int dseconds = 0;
+        private string alarmName = "null";
+
+        System.Timers.Timer timer;
+        // private Func<string, int, int> alarmTimeoutFun = (alarmName, alarmID) => 0;
+        private Func<string, int, int> alarmTimeoutFun; // = (alarmName, alarmID) => 0;
+
+        public AlarmClock(Func<string, int, int> alarmTimeoutFun, string alarmName, int alarmID)
+        {
+            this.alarmTimeoutFun = alarmTimeoutFun;
+            this.alarmName = alarmName;
+            this.alarmID = alarmID;
+            Console.WriteLine("Alarm [{0}] register...", this.alarmName);
+
+            //set timer property
+            timer = new System.Timers.Timer();
+            timer.Interval = 900;
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        public bool stopAlarm()
+        {
+            if (this.alarmTimeoutFun == null)
+            {
+                return false;
+            }
+
+            if (timer == null)
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("AlarmClock [{0}] stop", this.alarmName);
+                timer.Stop();
+                return true;
+            }
+        }
+
+        public bool startAlarmClock(int seconds)
+        {
+            if (this.alarmTimeoutFun == null)
+            {
+                Console.WriteLine("[Error] it does not register this alarm");
+                return false;
+            }
+
+            startTime = DateTime.Now;
+            this.dseconds = seconds;
+            Console.WriteLine("AlarmClock {0} [{1}] duration: {2} seconds start ", DateTime.Now, this.alarmName, this.dseconds);
+            timer.Start();
+            return true;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var timeDiff = DateTime.Now.Subtract(startTime).TotalSeconds;
+            if (timeDiff > dseconds)
+            {
+                Console.WriteLine("AlarmClock {0} [{1}] trigger", DateTime.Now, this.alarmName);
+                timer.Stop();
+
+                if (alarmTimeoutFun != null)
+                {
+                    Console.WriteLine("AlarmClock [{0}] Callback", this.alarmName);
+                    alarmTimeoutFun.Invoke(this.alarmName, this.alarmID);
+                }
+            }
+        }
+
+        ~AlarmClock()
+        {
+            Console.WriteLine("AlarmClock {0} release...", this.alarmName);
+            timer.Elapsed -= Timer_Elapsed;
+        }
+
+        public void recount()
+        {
+            startTime = DateTime.Now;
+        }
+    }
+}
